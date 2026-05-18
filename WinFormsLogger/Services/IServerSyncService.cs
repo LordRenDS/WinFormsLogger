09b014b12aadc03b7e4b159a0031940f6e3ec150
Ren <sergeysolovyov2016@gmail.com>
2026-05-18 22:36:30 +0200

    - add syncstatus enum and update iserversyncservice interface

--- WinFormsLogger/Services/IServerSyncService.cs
+++ WinFormsLogger/Services/IServerSyncService.cs
@@ -2,8 +2,17 @@ using System.Threading.Tasks;
 
 namespace WinFormsLogger.Services;
 
+public enum SyncStatus
+{
+    Success,
+    NoUnsyncedData,
+    NotAuthenticated,
+    PartiallyFailed,
+    Failed
+}
+
 public interface IServerSyncService
 {
     Task<string> LoginAsync(string username, string password);
-    Task SyncAsync();
+    Task<SyncStatus> SyncAsync();
 }
